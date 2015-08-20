0ad213fc45fcd6f13df9e3e513710f0a49bfe70a
fualkner <pbseverin@gmail.com>
2015-08-19 23:16:39 -0500

    Breaking down social barriers apparently involves erecting new ones. I guess. Added TagGroup.

--- cataloger/models.py
+++ cataloger/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Tag(models.Model):
-	title = models.CharField(max_length=200)
+class TagGroup(models.Model):
+	title = models.CharField(max_length=100)
 	def __str__(self):
 		return self.title
+
+class Tag(models.Model):
+	group = models.ForeignKey(TagGroup)
+	title = models.CharField(max_length=200)
+	def __str__(self):
+		return str(self.group) + ": " + self.title
 
 class Author(models.Model):
 	first_name = models.CharField(max_length=100)
